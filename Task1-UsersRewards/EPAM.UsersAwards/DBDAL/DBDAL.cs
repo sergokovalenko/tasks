cd4a13131454aa6509961f55efd7800c5e147435
Sergei_Kovalenko <sergokovalenko.dev@gmail.com>
2018-07-12 18:27:53 +0400

    Added basic intefaces, entities

---
+++ Task1-UsersRewards/EPAM.UsersAwards/DBDAL/DBDAL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersAward.DAL.AbstractDAL;
+using UsersAward.Entities;
+
+namespace UsersAward.DAL.DBDAL
+{
+    public class DBDAL : IAbstractDAL
+    {
+        private static string connectionString;
+
+        public DBDAL()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["UsersAwardsDB"].ConnectionString;
+        }
+
+        public bool AddAward(AwardDTO award)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO Award (Id, Title, Description) VALUES(@Id,@Title,@Description)", connection);
+
+                command.Parameters.AddWithValue("@Id", award.Id);
+                command.Parameters.AddWithValue("@Title", award.Title);
+                command.Parameters.AddWithValue("@Description", award.Description);
+                connection.Open();
+                int countRow = command.ExecuteNonQuery();
+
+                return countRow == 1;
+            }
+        }
+
+        public bool AddUser(UserDTO user)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO User (Id, Name, Birthdate) VALUES(@Id,@Name,@Birthdate)", connection);
+
+                command.Parameters.AddWithValue("@Id", user.Id);
+                command.Parameters.AddWithValue("@Name", user.Name);
+                command.Parameters.AddWithValue("@Birthdate", user.BirthDate);
+                connection.Open();
+                int countRow = command.ExecuteNonQuery();
+
+                return countRow == 1;
+            }
+        }
+
+        public bool DeleteAward(int awardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AwardDTO> GetAllAwards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserDTO> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateAward(AwardDTO updatedAward)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUser(UserDTO updatedUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task1-UsersRewards/EPAM.UsersAwards/UsersAward.Entities/AwardDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersAward.Entities
+{
+    public class AwardDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
