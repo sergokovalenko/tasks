e2dc4503e5114641a92970df449fc3c48af1b917
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-07-15 14:38:17 +0400

    Model Validation

--- Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.Web/Models/UserModels/DisplayUserVM.cs
+++ Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.Web/Models/UserModels/DisplayUserVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using UsersAward.Entities;
@@ -11,31 +12,9 @@ namespace UsersAward.PLL.Web.Models.UserModels
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        [Display(Name = "Birthday")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime BirthDate { get; set; }
         //public List<AwardDTO> Awards { get; set; }
-
-        public static explicit operator UserDTO(DisplayUserVM vm)
-        {
-            return new UserDTO()
-            {
-                Id = vm.Id,
-                Name = vm.Name,
-                Age = vm.Age,
-                Awards = new List<AwardDTO>(),
-                ImageId = Guid.Empty,
-                BirthDate = vm.BirthDate
-            };
-        }
-
-        public static implicit operator DisplayUserVM(UserDTO vm)
-        {
-            return new DisplayUserVM()
-            {
-                Id = vm.Id,
-                Name = vm.Name,
-                Age = vm.Age,
-                BirthDate = vm.BirthDate
-            };
-        }
     }
 }
