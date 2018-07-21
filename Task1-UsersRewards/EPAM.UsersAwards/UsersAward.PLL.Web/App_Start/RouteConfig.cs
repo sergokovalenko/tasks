1373471a76c926554d6d41e92524c48628c84bf4
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-07-21 13:39:49 +0400

    Routing for simple methods

--- Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.Web/App_Start/RouteConfig.cs
+++ Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,66 @@ namespace UsersAward.PLL.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "users/",
+                defaults: new { controller = "User", action = "Index" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "create-user/",
+                defaults: new { controller = "User", action = "Create" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "user/{id}",
+                defaults: new { controller = "User", action = "Details" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "user/{id}/edit",
+                defaults: new { controller = "User", action = "Edit" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "user/{id}/delete",
+                defaults: new { controller = "User", action = "Delete" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "awards/",
+                defaults: new { controller = "Awards", action = "Index" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "create-award/",
+                defaults: new { controller = "Awards", action = "Create" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "award/{id}",
+                defaults: new { controller = "Awards", action = "Details" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "award/{id}/edit",
+                defaults: new { controller = "Awards", action = "Edit" }
+                );
+
+            routes.MapRoute(
+                name: null,
+                url: "award/{id}/delete",
+                defaults: new { controller = "Awards ", action = "Delete" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
