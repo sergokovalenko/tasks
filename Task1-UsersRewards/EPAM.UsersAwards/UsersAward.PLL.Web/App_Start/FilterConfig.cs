df2206eec3100d949b7b8d3462521a499a24c6a5
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-07-24 09:44:15 +0400

    Global filter for exceptions

---
+++ Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsersAward.PLL.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            var errorFilter = new HandleErrorAttribute();
+            errorFilter.ExceptionType = typeof(Exception);
+            errorFilter.View = "Error";
+
+            filters.Add(errorFilter);
+        }
+    }
+}
