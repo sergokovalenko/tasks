97adda1d7c9b51939328493689c7664fa912ce01
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-07-26 13:33:29 +0400

    Removed useless usings

--- Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.WebNew/App_Start/Ninject.Web.Common.cs
+++ Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.WebNew/App_Start/Ninject.Web.Common.cs
@@ -5,13 +5,10 @@ namespace UsersAward.PLL.Web.App_Start
 {
     using System;
     using System.Web;
-
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
     using Ninject;
     using Ninject.Web.Common;
     using Ninject.Web.Common.WebHost;
-    using UsersAward.BLL.AbstractBLL;
     using UsersAward.PLL.Web.Models;
 
     public static class NinjectWebCommon 
