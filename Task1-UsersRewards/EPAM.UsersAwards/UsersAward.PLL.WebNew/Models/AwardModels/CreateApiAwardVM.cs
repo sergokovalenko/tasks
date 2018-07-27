310f51d08d17226150c6a1f7c85673428a6a5b87
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-07-27 10:42:57 +0400

    hot api fix

---
+++ Task1-UsersRewards/EPAM.UsersAwards/UsersAward.PLL.WebNew/Models/AwardModels/CreateApiAwardVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsersAward.PLL.Web.Models.AwardModels
+{
+    public class CreateApiAwardVM
+    {
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"[a-zA-Z0-9]([ -]?[0-9a-zA-Z]){0,49}")]
+        [Remote("IsAwardAllowed", "Validation")]
+        public string Title { get; set; }
+
+        [StringLength(250)]
+        public string Description { get; set; }
+    }
+}
