d73c49774996e64d198fc1812de57ca39cc21ab6
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-08-29 15:54:43 +0400

    EPMRDRUFE-609

--- Task7-JS Core/src/js/first-task.js
+++ Task7-JS Core/src/js/first-task.js
@@ -1,24 +1,3 @@
-var APP = APP || {};
-APP.createNamespace = function (path) {
-    var parts = path.split('.'),
-        parent = APP,
-        i = 0;
-
-    if (parts[i] === 'APP') {
-        parts = parts.slice(1);
-    }
-
-    for (i = 0; i < parts.length; i += 1) {
-        if (typeof parent[parts[i]] === "undefined") {
-            parent[parts[i]] = {};
-        }
-
-        parent = parent[parts[i]];
-    }
-
-    return parent;
-}
-
 APP.createNamespace("APP.tasks.first");
 
 APP.tasks.first = (function () {
