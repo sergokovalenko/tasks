85242bf6f02cebacaa694e43188529306e71ad77
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-08-30 17:22:40 +0400

    Added array

--- Task7-JS Core/src/js/third-task.js
+++ Task7-JS Core/src/js/third-task.js
@@ -8,7 +8,66 @@ APP.tasks.third = (function () {
         regexp = /M{1,4}|yy(?:yy)?|([HMhmsd])\1?/g,
         submitBtn = {},
         dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        formatFlags = {
+            yy: function (date) {
+                return String(date.getFullYear()).substr(2);
+            },
+            yyyy: function (date) {
+                return withZero(date.getFullYear(), 4);
+            },
+            M: function (date) {
+                return date.getMonth() + 1;
+            },
+            MM: function (date) {
+                return withZero(date.getMonth() + 1);
+            },
+            MMM: function (date, i18n) {
+                return i18n.monthNamesShort[date.getMonth()];
+            },
+            MMMM: function (date, i18n) {
+                return i18n.monthNames[date.getMonth()];
+            },
+            d: function (date) {
+                return date.getDay();
+            },
+            dd: function (date) {
+                return withZero(date.getDay());
+            },
+            H: function (date) {
+                return date.getHours();
+            },
+            HH: function (date) {
+                return withZero(date.getHours());
+            },
+            h: function (date) {
+                return date.getHours();
+            },
+            hh: function (date) {
+                return withZero(date.getHours());
+            },
+            m: function (date) {
+                return date.getMinutes();
+            },
+            mm: function (date) {
+                return withZero(date.getMinutes());
+            },
+            s: function (date) {
+                return date.getSeconds();
+            },
+            ss: function (date) {
+                return withZero(date.getSeconds());
+            }
+        };
+
+    function withZero(val, len) {
+        val = String(val);
+        len = len || 2;
+        while (val.length < len) {
+            val = '0' + val;
+        }
+        return val;
+    }
 
     function getResultForThirdTask() {
 
@@ -30,7 +89,7 @@ APP.tasks.third = (function () {
         submitBtn.addEventListener('click', function () {
             showResult();
         });
-        
+
     }
 
     return {
