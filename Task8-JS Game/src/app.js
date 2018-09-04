3674adc294ab25a52818e6cd652f8b68e91cf093
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-09-04 18:56:02 +0400

    EPMRDRUFE-740

---
+++ Task8-JS Game/src/app.js
@@ -0,0 +1,10 @@
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+window.onload = function () {
+  console.log('number');
+};
