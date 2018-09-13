4724dc20f16e4115310f680f40144c5c3d8352d8
Sergei_Kovalenko <sergokovalenko.dev@gmail.com>
2018-09-13 20:15:51 +0400

    After consultation

--- Task8-JS Game/src/js/entities/sprite.js
+++ Task8-JS Game/src/js/entities/sprite.js
@@ -1,24 +1,11 @@
 import Entity from './entity';
 
-function Sprite(url, x, y, width, height, frames) {
+function Sprite(url, x, y, width, height, frameCount) {
   Entity.call(this, x, y, width, height);
   this.url = url;
-  this.frames = frames;
+  this.frameCount = frameCount;
   this.index = 0;
+  this.animationTimer = 1;
 }
-
-Sprite.prototype.draw = function draw(ctx) {
-  ctx.drawImage(
-    window.resources.getImg(this.spriteInfo.url),
-    this.spriteInfo.position.x,
-    this.spriteInfo.position.y,
-    this.spriteInfo.size.width,
-    this.spriteInfo.size.height,
-    this.position.x,
-    this.position.y,
-    this.size.width,
-    this.size.height,
-  );
-};
 
 export default Sprite;
