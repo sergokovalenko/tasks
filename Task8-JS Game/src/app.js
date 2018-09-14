92f8f727a65c9100f769b91abddc8a66767d8892
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-09-14 10:05:16 +0400

    Game pause

--- Task8-JS Game/src/app.js
+++ Task8-JS Game/src/app.js
@@ -1,11 +1,16 @@
 import './js/helpers/keyHandler';
 import './js/helpers/resourceLoader';
 import * as all from './assets/img/allObj.png';
-import init from './js/game';
+import { initialize as init, pauseGame } from './js/game';
 
 window.onload = () => {
   window.resources.load(all);
   setTimeout(() => {
     init(all);
   }, 1000);
+
+  const btnPause = document.getElementById('pause-btn');
+  btnPause.addEventListener('click', () => {
+    pauseGame();
+  });
 };
