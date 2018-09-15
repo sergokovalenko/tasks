2d326c331a87e22d5153213fb48bda487087e256
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-09-15 17:57:45 +0400

    Changed methods from 'apply' to 'call'

--- Task8-JS Game/src/js/entities/wall.js
+++ Task8-JS Game/src/js/entities/wall.js
@@ -1,7 +1,7 @@
 import Entity from './entity';
 
 function Wall(x, y, width, height, sprite, isDestr = true, hasColl = true, hasCollWithBul = true) {
-  Entity.apply(this, [x, y, width, height]);
+  Entity.call(this, x, y, width, height);
   this.spriteInfo = sprite;
   this.isDestr = isDestr;
   this.hasColl = hasColl;
