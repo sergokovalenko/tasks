317da2c348bb613a0a660311a5d41d78066c8b36
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-09-19 19:21:49 +0400

    Fake dal realization

---
+++ Task9-jQuery/src/js/fakeDal.js
@@ -0,0 +1,60 @@
+import data from './data.json';
+
+class Dal {
+  constructor() {
+    this.data = data;
+  }
+  getAll() {
+    return this.data;
+  }
+
+  getElementById(id) {
+    return this.data.find(el => el.id === id);
+  }
+
+  removeElement(id) {
+    const index = this.data.findIndex(el => el.id === id);
+
+    if (index < 0) {
+      return false;
+    }
+
+    this.data.splice(index, 0);
+    return true;
+  }
+
+  add(el) {
+    this.data.push(el);
+  }
+
+  update(el) {
+    const index = this.data.findIndex(row => row.id === el.id);
+
+    if (index < 0) {
+      return false;
+    }
+
+    const keys = Object.keys(el);
+
+    for (let i = 0; i < keys.length; i += 1) {
+      const key = keys[i];
+      this.data[index][key] = el[key];
+    }
+
+    return true;
+  }
+
+  find(expression) {
+    let condition = expression;
+
+    if (condition) {
+      condition = new RegExp(condition, 'gi');
+    }
+
+    const resultArr = this.data.filter(el => el.name.search(condition) >= 0);
+
+    return resultArr;
+  }
+}
+
+export default Dal;
