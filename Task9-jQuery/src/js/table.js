6e0f14f221a0fd1ff519734d283facc0d73cd822
Sergei_Kovalenko <sergokovalenko.dev@gmail.com>
2018-09-20 16:39:49 +0400

    Delete modal

--- Task9-jQuery/src/js/table.js
+++ Task9-jQuery/src/js/table.js
@@ -1,24 +1,36 @@
-import _ from 'underscore';
 import $ from 'jquery';
 import Bll from './Bll';
+import { productRowTemplateFunc, tableTemplateFunc } from './templates';
+import DeleteModal from './components/deleteModal';
 
 class Table {
   constructor() {
     this.logic = new Bll();
+    this.deleteModal = new DeleteModal(this.logic, 'modal-container');
+    this.edit = function edit(productId) {
+      console.log(`edit ${productId}`);
+    };
+    this.delete = function del(productId) {
+      this.deleteModal.show(productId);
+      console.log(`delete ${productId}`);
+    };
   }
 
   drawFullTable() {
     const productList = this.logic.getAll();
-    const productRowHtml = $('#tableRowTemplate').html();
-    const productRowTemplateFunc = _.template(productRowHtml);
-    const tableHtml = $('#tableTemplate').html();
-    const tableTemplateFunc = _.template(tableHtml);
 
     const table = tableTemplateFunc({
       productList,
       productRowTemplateFunc,
     });
     $('#container').html(table);
+
+    $('.table').on('click', '.delete, .edit', (e) => {
+      const $btn = $(e.target);
+      const action = $btn.attr('data-action');
+
+      this[action]($btn.attr('data-id'));
+    });
   }
 }
 
