11b38be624c351b2d0103881c287e2403ce2ecc3
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-09-20 18:49:47 +0400

    EPMRDRUFE-984 Price convertation Sorting

--- Task9-jQuery/src/app.js
+++ Task9-jQuery/src/app.js
@@ -1,9 +1,21 @@
 import $ from 'jquery';
 import './assets/scss/main.scss';
 import Table from './js/table';
+import './js/utilities/priceConvertor';
 
 $('document').ready(() => {
+  const $searchBtn = $('#searchBtn');
+  const $searchField = $('#search-field');
   const table = new Table();
+
+  $searchBtn.on('click', () => {
+    const expr = $searchField.val().trim();
+    if (!expr) {
+      table.drawFullTable();
+    } else {
+      table.filter(expr);
+    }
+  });
 
   table.drawFullTable();
 });
--- Task9-jQuery/src/js/templates.js
+++ Task9-jQuery/src/js/templates.js
@@ -5,8 +5,10 @@ const productRowHtml = $('#tableRowTemplate').html();
 const tableHtml = $('#tableTemplate').html();
 const deleteModalHtml = $('#modalDeleteTemplate').html();
 const changeModalHtml = $('#modalChangeTemplate').html();
+const tableRowRedrawHtml = $('#tableRowRedrawTemplate').html();
 
 export const productRowTemplateFunc = _.template(productRowHtml);
 export const tableTemplateFunc = _.template(tableHtml);
 export const deleteModalTemplateFunc = _.template(deleteModalHtml);
 export const changeModalTemplateFunc = _.template(changeModalHtml);
+export const tableRowRedrawTemplate = _.template(tableRowRedrawHtml);
