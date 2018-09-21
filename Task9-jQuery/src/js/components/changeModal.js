6f585aea1ec2c36425b1486654b3e1715f290bbf
Sergei_Kovalenko <sergokovalenko.dev@gmail.com>
2018-09-21 13:28:15 +0400

    Validator inccorrecte

--- Task9-jQuery/src/js/components/changeModal.js
+++ Task9-jQuery/src/js/components/changeModal.js
@@ -1,11 +1,35 @@
 import $ from 'jquery';
 import { changeModalTemplateFunc } from './../templates';
+import validator from './../utilities/valid';
+
+function setEvents() {
+  $('#close').on('click', () => {
+    this.modal.css('display', 'none');
+  });
+
+  $('#name').on('blur', function nameClick() {
+    const value = this.innerText;
+    const hasError = validator.validate({
+      name: value,
+    });
+    const errorBlock = $('.error-name:eq(0)');
+    if (hasError) {
+      errorBlock.html('');
+      console.log('invalid');
+    } else {
+      errorBlock.html(validator.messages[0]);
+      console.log(validator.messages);
+      console.log('valid');
+    }
+  });
+}
 
 class ChangeModal {
   constructor(logic, elemId) {
     this.logic = logic;
     this.elem = $(`#${elemId}`);
     this.modal = null;
+    setEvents.bind(this);
   }
 
   show(id) {
@@ -17,10 +41,10 @@ class ChangeModal {
     this.modal = $('#modalWindow');
     this.modal.css('display', 'block');
 
-
-    $('#close').on('click', () => {
-      this.modal.css('display', 'none');
-    });
+    setEvents.bind(this)();
+    // $('#close').on('click', () => {
+    //   this.modal.css('display', 'none');
+    // });
   }
 
   hide() {
