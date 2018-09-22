5900f7bd69920772179161b3e4ee0e447905a375
Sergei_Kovalenko <sergokovalenko.dev@gmail.com>
2018-09-22 18:30:16 +0400

    Buttons validation

--- Task9-jQuery/src/app.js
+++ Task9-jQuery/src/app.js
@@ -2,11 +2,14 @@ import $ from 'jquery';
 import './assets/scss/main.scss';
 import Table from './js/table';
 import './js/utilities/priceConvertor';
+import ChangeModal from './js/components/changeModal';
 
 $('document').ready(() => {
   const $searchBtn = $('#searchBtn');
   const $searchField = $('#search-field');
+  const $addhBtn = $('#addNew');
   const table = new Table();
+  const changeModal = new ChangeModal(table.logic, 'modal-container');
 
   $searchBtn.on('click', () => {
     const expr = $searchField.val().trim();
@@ -15,6 +18,14 @@ $('document').ready(() => {
     } else {
       table.filter(expr);
     }
+  });
+
+  $addhBtn.on('click', () => {
+    changeModal.show(-1000);
+    $('#superBtn').on('click', (e) => {
+      e.preventDefault();
+      changeModal.hide();
+    });
   });
 
   table.drawFullTable();
--- Task9-jQuery/src/js/components/changeModal.js
+++ Task9-jQuery/src/js/components/changeModal.js
@@ -2,65 +2,103 @@ import $ from 'jquery';
 import { changeModalTemplateFunc } from './../templates';
 import validator from './../utilities/valid';
 
-function setEvents() {
-  $('#close').on('click', () => {
+function blurEvent(field, fieldName, $submitBtn) {
+  const value = $(field).val();
+  const hasError = validator.validate({
+    name: value,
+  });
+  const errorBlock = $(`.error-${fieldName}:eq(0)`);
+  if (hasError) {
+    errorBlock.html(validator.messages[0]);
+    $submitBtn.prop('disabled', true);
+  } else {
+    errorBlock.html('');
+    const isValid = $('.error').toArray().every(el => !el.innerHTML);
+    if (isValid) {
+      $submitBtn.prop('disabled', false);
+    }
+  }
+}
+
+function mapObject(formId) {
+  const product = {};
+  const serialized = $(`#${formId}`).serializeArray();
+  serialized.forEach((el) => {
+    product[el.name] = el.value;
+  });
+  product.city = [];
+  $('input[name="city"]').each((i, el) => {
+    const $el = $(el);
+    if ($el.prop('checked')) {
+      product.city.push($el.val());
+    }
+  });
+
+  return product;
+}
+
+function setEvents(callback) {
+  // TODO: делегирование событий
+  const $superBtn = $('#superBtn');
+  const $close = $('#close');
+  const $name = $('#name');
+  const $email = $('#email');
+  const $count = $('#count');
+  const $price = $('#price');
+  const $selectAllBox = $('#select-all');
+
+  $close.on('click', () => {
     this.modal.css('display', 'none');
   });
 
-  $('#name').on('blur', function nameClick() {
-    const value = $(this).val();
-    const hasError = validator.validate({
-      name: value,
-    });
-    const errorBlock = $('.error-name:eq(0)');
-    if (hasError) {
-      errorBlock.html(validator.messages[0]);
-    } else {
-      errorBlock.html('');
-    }
+  $name.on('blur', function nameClick() {
+    blurEvent(this, 'name', $superBtn);
   });
 
-  $('#email').on('blur', function nameClick() {
-    const value = $(this).val();
-    const hasError = validator.validate({
-      email: value,
-    });
-    const errorBlock = $('.error-email:eq(0)');
-    if (hasError) {
-      errorBlock.html(validator.messages[0]);
-    } else {
-      errorBlock.html('');
-    }
+  $email.on('blur', function emailClick() {
+    blurEvent(this, 'email', $superBtn);
   });
 
-  $('#count').on('blur', function nameClick() {
-    const value = $(this).val();
-    const hasError = validator.validate({
-      count: value,
-    });
-    const errorBlock = $('.error-count:eq(0)');
-    if (hasError) {
-      errorBlock.html(validator.messages[0]);
-    } else {
-      errorBlock.html('');
-    }
+  $count.on('blur', function countClick() {
+    blurEvent(this, 'count', $superBtn);
   });
 
-  $('#count').bind('input propertychange', function a() {
+  $count.bind('input propertychange', function a() {
     const input = $(this);
     input.val(input.val().replace(/[^0-9]/g, ''));
   });
 
-  $('#price').on('blur', function nameClick() {
-    const value = $(this).val();
-    const hasError = validator.validate({
-      price: value,
+  $price.on('blur', function nameClick() {
+    blurEvent(this, 'price', $superBtn);
+    const product = mapObject('modalForm');
+
+    console.log(product);
+  });
+
+  $selectAllBox.on('click', () => {
+    const flag = $selectAllBox.prop('checked');
+
+    $('input[name="city"]').each((i, el) => {
+      $(el).prop('checked', flag);
     });
-    const errorBlock = $('.error-price:eq(0)');
-    if (hasError) {
-      errorBlock.html(validator.messages[0]);
-    } else {
-      errorBlock.html('');
+  });
+
+  $superBtn.on('click', (e) => {
+    e.preventDefault();
+
+    if (!$superBtn.prop('disabled')) {
+      const product = mapObject('modalForm');
+
+      console.log(product);
+      if (!validator.validate(product)) {
+        callback(product);
+        console.log('Added');
+      } else {
+        $name.blur();
+        $email.blur();
+        $count.blur();
+        $price.blur();
+      }
     }
   });
 }
@@ -73,16 +111,29 @@ class ChangeModal {
     setEvents.bind(this);
   }
 
-  show(id) {
-    const product = this.logic.getElementById(id);
-    const modal = changeModalTemplateFunc({
-      product,
-    });
+  show(id, callback) {
+    let modal;
+    if (id <= -1000) {
+      const product = {
+        name: '',
+        price: '',
+        email: '',
+        count: '',
+      };
+      modal = changeModalTemplateFunc({
+        product,
+      });
+    } else {
+      const product = this.logic.getElementById(id);
+      modal = changeModalTemplateFunc({
+        product,
+      });
+    }
     this.elem.html(modal);
     this.modal = $('#modalWindow');
     this.modal.css('display', 'block');
 
-    setEvents.bind(this)();
+    setEvents.call(this, callback);
   }
 
   hide() {
