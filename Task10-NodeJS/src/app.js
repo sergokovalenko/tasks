124b1da8e3331f63ec17f75239895ef817bf3eaf
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-10-03 13:07:51 +0400

    remove, filter apis

--- Task10-NodeJS/src/app.js
+++ Task10-NodeJS/src/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const productRouter = require('./js/routers/productRouter');
 const productsRouter = require('./js/routers/productsRouter');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/product', productRouter);
