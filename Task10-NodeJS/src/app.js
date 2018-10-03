ce9281e33b7ff0e925f5c1d0c0c22e949dfb156f
sergokovalenko <sergokovalenko.dev@gmail.com>
2018-10-03 12:56:08 +0400

    fetch getAll and getById

--- Task10-NodeJS/src/app.js
+++ Task10-NodeJS/src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const productRouter = require('./js/routers/productRouter');
 const productsRouter = require('./js/routers/productsRouter');
+// const cors = require('cors');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+// app.use(cors());
 app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/product', productRouter);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+app.listen(3001, () => {
+  console.log('Server started on port 3001!');
 });
